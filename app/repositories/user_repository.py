4f73e5f722e24699ca09a4ecc25efced6f18f59d
Khaled Benoukhiye <khaledbeno922@gmail.com>
2026-04-05 07:34:49 +0200

    Fix problem BD conection with users

---
+++ app/repositories/user_repository.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Session
+from app.db.models.user import User
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+
+def create_user(db: Session, username: str, email: str, password: str):
+    user = User(
+        username=username,
+        email=email,
+        password=password
+    )
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return user
